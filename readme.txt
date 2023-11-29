754650fc330ce1f42b9470542e440e4aa7bdb349
Rilwanu Isyaku <rilwanu.kmphitech@gmail.com>
2023-11-29 13:25:17 +0100

    first commit

---
+++ readme.txt
@@ -0,0 +1,82 @@
+=== Post Stats ===
+Contributors: cedbv
+Donate link: http://www.boverie.eu/
+Tags: reading time,stats,word count
+Requires at least: 2.9
+Tested up to: 3.0
+Stable tag: 1.1
+
+Statistics about posts length and reading time on dashboard and/or sidebar widget.
+Optionally, display estimated reading time before each post.
+
+== Description ==
+
+Statistics about posts length and reading time on dashboard and/or sidebar widget (see screenshots).    
+Optionally, displays additional statistics before each post.
+
+= Stats display in the widget =
+* Total number of words in blog posts
+* Minimal and maximal post length.
+* Shortest and longest post (title and permalink)
+* Estimated reading time of the blog posts
+
+= Before each post =
+* Length
+* Estimated reading time
+
+== Installation ==
+
+1. Upload `poststats` directory to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Configure the plugin's settings.
+
+== Frequently Asked Questions ==
+
+= What are the requirements ? =
+* Wordpress 2.9.2 or newer
+* PHP 5
+
+== Screenshots ==
+
+1. Widget displays on administration dashboard.
+2. Options page
+3. Post length and estimated reading time inserted automatically before each post content. 
+
+== Changelog ==
+
+= 1.1 =
+* New cache
+
+= 1.0.1 =
+* Fix l10n.
+
+= 1.0 =
+* First public version.
+
+= 0.4 =
+* Add settings link
+* Queries optimization.
+* Bug fix.
+
+= 0.3 =
+* Adding configuration option.
+
+= 0.2 =
+* Adding sidebar widget.
+* Common widget content for dashboard and sidebar.
+
+= 0.1.1 =
+* Widget content improvement
+
+= 0.1 =
+* First version.
+* Adding dashboard widget
+
+== Upgrade Notice ==
+
+= 1.1 =
+New cache :   
+If you have a huge blog, big performances improvement.
+
+= 1.0 =
+Install plugin as usual.
---
+++ wakatafiz.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * Plugin Name: WakataFiz
+ * Description: A word count plugin for single post statistics.
+ * Version: 1.0
+ * Author: Rilwanu
+ * Author URI: https://github.com/ridwanishaq
+ * Text Domain: wakatafizdomain
+ * Domain Path: /languages
+ * 
+ */
+
+class WakataFiz {
+    
+    public function __construct()
+    {
+        add_action('admin_menu', array($this, 'wfiz_setting_link'));
+        add_action('admin_init', array($this, 'settings'));
+        add_filter('the_content', array($this, 'ifWrap'), 10, 1);
+        add_action('init', array($this, 'languages'));
+    }
+
+    public function languages() {
+        load_plugin_textdomain( 'wakatafizdomain', false, dirname(plugin_basename( __FILE__ )) . '/languages');
+    }
+
+    public function ifWrap($content) {
+        if( is_main_query() && is_single() && 
+        (
+            get_option('wfiz_wordcount', '1') ||
+            get_option('wfiz_charactercount', '1') || 
+            get_option('wfiz_readtime', '1')
+        )) {
+            return $this->createHTML($content);
+        }
+        return $content;
+    }
+
+    public function createHTML($content) {
+        $html = '<h3>'. esc_html(get_option('wfiz_headline', 'Post Statistics')) .'</h3><p>';
+
+        // get word count once because both wordcount and read time will need it.
+        if(get_option('wfiz_wordcount', '1') || get_option( 'wfiz_readtime', '1' )) {
+            $wordCount = str_word_count(strip_tags($content));
+        }
+
+        if(get_option('wfiz_wordcount', '1')) {
+            $html .= esc_html__('This post has', 'wakatafizdomain'). ' '. $wordCount . ' ' . esc_html__('words', 'wakatafizdomain') . '.<br/>';
+        }
+
+        if(get_option('wfiz_charactercount', '1')) {
+            $html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br/>';
+        }
+
+        if(get_option('wfiz_readtime', '1')) {
+            $html .= 'This post will take about ' . round($wordCount/255) . ' minute(s) to read.<br/>';
+        }
+
+        $html  .= '</p>';
+
+        if(get_option('wfiz_location', '0') == '0') {
+            return $html . $content;
+        }
+        return $content . $html;
+    }
+
+    public function settings() {
+        add_settings_section('wfiz_first_section', null, null, 'wfiz-word-count-settings-page');
+        
+        // Location Field
+        add_settings_field('wfiz_location', 'Display Location', array($this, 'locationHTML'), 'wfiz-word-count-settings-page', 'wfiz_first_section');
+        register_setting('wordcountplugin', 'wfiz_location', array('sanitize_callback' => array($this, 'sanitizeLocation'), 'default' => '0'));
+
+        // Headline Field
+        add_settings_field('wfiz_headline', 'Headline Text', array($this, 'headlineHTML'), 'wfiz-word-count-settings-page', 'wfiz_first_section');
+        register_setting('wordcountplugin', 'wfiz_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistic'));
+
+        // Word Count
+        add_settings_field('wfiz_wordcount', 'Word count', array($this, 'checkboxHTML'), 'wfiz-word-count-settings-page', 'wfiz_first_section', array('theName' => 'wfiz_wordcount'));
+        register_setting('wordcountplugin', 'wfiz_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+        // Character Count
+        add_settings_field('wfiz_charactercount', 'Character count', array($this, 'checkboxHTML'), 'wfiz-word-count-settings-page', 'wfiz_first_section', array('theName' => 'wfiz_charactercount'));
+        register_setting('wordcountplugin', 'wfiz_charactercount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+        // Read Time
+        add_settings_field('wfiz_readtime', 'Read time', array($this, 'checkboxHTML'), 'wfiz-word-count-settings-page', 'wfiz_first_section', array('theName' => 'wfiz_readtime'));
+        register_setting('wordcountplugin', 'wfiz_readtime', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+    }
+
+    public function sanitizeLocation($input) {
+        if($input != '0' && $input != '1'){
+            add_settings_error( 'wfiz_location', 'wfiz_location_error', 'Display location must be either beginning or end.');
+            return get_option( 'wfiz_location' );
+        }
+        return $input;
+    }
+
+    public function locationHTML() {
+        ?>
+            <select name='wfiz_location'>
+                <option value='0' <?php selected(get_option('wfiz_location', '0')) ?> >Beginning of post</option>
+                <option value='1' <?php selected(get_option('wfiz_location', '1')) ?>>End of post</option>
+            </select>
+        <?php
+    }
+
+    public function headlineHTML() { ?>
+        <input type="text" name="wfiz_headline" value="<?php echo esc_attr(get_option('wfiz_headline')) ?>">
+    <?php 
+    }
+
+    // reusable checkbox
+    public function checkboxHTML($args) { ?>
+        <input type="checkbox" name="<?php echo $args['theName'] ?>" value="1" <?php checked(get_option($args['theName']), '1') ?>>
+    <?php
+    }
+
+    public function wfiz_setting_link() {
+        add_options_page('WakataFIZ Settings', esc_html__('Wakata FIZ', 'wakatafizdomain'),'manage_options','wfiz-word-count-settings-page', array($this, 'wfiz_settings_page_html'));
+    }
+    
+    public function wfiz_settings_page_html() {
+    
+        ?>
+        <div class='wrap'>
+            <h1>Wakata FIZ Settings</h1>
+            <form action='options.php' method='POST'>
+            <?php
+                settings_fields('wordcountplugin');
+                do_settings_sections('wfiz-word-count-settings-page');
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+}
+
+new WakataFiz();
